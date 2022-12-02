e2b50d555cfaabc417f44437e45bf583f850f3e9
scyllacore <yms8795@gmail.com>
2022-12-02 13:16:34 +0900

    feat : 다리를 생성해주는 클래스 구현.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,23 +1,33 @@
 package bridge;
 
+import bridge.constant.GameConstant;
+import bridge.controller.MapConverter;
+import bridge.exception.IllegalArgument;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        validateSize(size);
+
+        return Stream.generate(bridgeNumberGenerator::generate)
+                .limit(size)
+                .map(MapConverter::getDirection)
+                .collect(Collectors.toList());
+    }
+
+    private void validateSize(int size) {
+        if (size >= GameConstant.MIN_SIZE && size <= GameConstant.MAX_SIZE) {
+            return;
+        }
+        IllegalArgument.handleException(IllegalArgument.WRONG_INPUT.getMessage());
     }
 }
